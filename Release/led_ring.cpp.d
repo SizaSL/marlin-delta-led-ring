f60db872a1b05777e9cd049b8ca64482a5285ba0
Newtonmin <newtonmin@gmail.com>
2017-04-06 19:03:11 +1200

    Initial commit

---
+++ Release/led_ring.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\Siza\AppData\Local\Temp\VMBuilds\led_ring\pro_16MHzatmega328\Release\led_ring.cpp.o: \
+ C:\Users\Siza\AppData\Local\Temp\VMBuilds\led_ring\pro_16MHzatmega328\Release\led_ring.cpp \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/arduino.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/binary.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/WCharacter.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/WString.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/HardwareSerial.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Stream.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Print.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Printable.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/USBAPI.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Arduino.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\variants\eightanaloginputs/pins_arduino.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\variants\eightanaloginputs/../standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Adafruit_NeoPixel/Adafruit_NeoPixel.h \
+ C:\Users\Siza\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Arduino.h
