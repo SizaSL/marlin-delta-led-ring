c4867cc86b7b967ebe06252818a71315c4a0c8ae
Newtonmin <newtonmin@gmail.com>
2017-04-15 13:29:41 +1200

    Update Python script for serial start
    * Updated serial start script for Linux
    * Added serial start script for Windows

---
+++ tools/mega_serial_start_win.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import serial, sys, time
+from serial.tools import list_ports
+from subprocess import call
+
+try:
+        mega_serial_port = next(list_ports.grep("Arduino Mega 2560"))
+        serial_port = serial.Serial(port=mega_serial_port.device,
+                                                                baudrate=57600,
+                                                                parity=serial.PARITY_NONE,
+                                                                stopbits=serial.STOPBITS_ONE,
+                                                                bytesize=serial.EIGHTBITS)
+        serial_port.isOpen()
+        #time.sleep(1)
+        serial_port.close()
+        time.sleep(5)
+        port_name = '-P' + mega_serial_port.device
+        call(['tools/avrdude_win', '-V', '-Ctools/avrdude.conf', '-v', '-r', '-patmega328p', '-carduino', port_name, '-b57600', '-Uflash:w:led-ring.hex:i'])
+except StopIteration:
+        print("No Arduino Mega device connected")
