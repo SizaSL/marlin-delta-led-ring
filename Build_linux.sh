344e720586b20476e5e2ba5579b2d88b4066ad89
Newtonmin <newtonmin@gmail.com>
2017-04-16 13:17:55 +1200

    Update test scripts

--- Build_linux.sh
+++ Build_linux.sh
@@ -1,3 +1,2 @@
 mkdir platformio-build
-chmod 666 /dev/ttyACM0
-platformio ci --verbose --board pro16MHzatmega328 --project-option="lib_extra_dirs=C:\Program Files (x86)\Arduino\libraries" --project-option="targets=upload" --project-option="upload_port=/dev/ttyUSB0" --build-dir=platformio-build --keep-build-dir led-ring
+platformio ci --verbose --board pro16MHzatmega328 --project-option="targets=upload" --project-option="upload_port=/dev/ttyUSB0" --build-dir=platformio-build --keep-build-dir led-ring
---
+++ tools/functional_test_linux.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import serial, sys, time
+from serial.tools import list_ports
+from subprocess import call
+
+try:
+	#mega_serial_port = next(list_ports.grep("Arduino Pro"))
+	serial_port = serial.Serial(port="/dev/ttyUSB0",
+								baudrate=115200,
+								parity=serial.PARITY_NONE,
+								stopbits=serial.STOPBITS_ONE,
+								bytesize=serial.EIGHTBITS)
+	serial_port.isOpen()
+	time.sleep(3)
+	serial_port.write("asdf")
+	time.sleep(6)
+	serial_port.close()
+except StopIteration:
+	print("No Arduino device connected")
