e89de19571cb6dbc0296ead1fcab60e0dbc78e8a
Newtonmin <newtonmin@gmail.com>
2017-04-16 12:10:01 +1200

    Update Linux build script

--- Build_linux.sh
+++ Build_linux.sh
@@ -1,4 +1,2 @@
 mkdir platformio-build
-platformio ci --verbose --board pro16MHzatmega328 --project-option="lib_extra_dirs=C:\Program Files (x86)\Arduino\libraries" --build-dir=platformio-build --keep-build-dir led-ring
-chmod 700 tools/mega_serial_start_linux.py tools/avrdude_linux
-python tools/mega_serial_start_linux.py
+platformio ci --verbose --board pro16MHzatmega328 --project-option="lib_extra_dirs=C:\Program Files (x86)\Arduino\libraries:upload_port=/dev/ttyAMA0" --build-dir=platformio-build --keep-build-dir led-ring
