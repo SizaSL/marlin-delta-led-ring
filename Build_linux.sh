ce2b929d8e9741596dc50193061079a886bea0c1
Newtonmin <newtonmin@gmail.com>
2017-04-16 12:35:24 +1200

    Update build scripts
    * Both Linux and Windows
    * Added upload option and ports

--- Build_linux.sh
+++ Build_linux.sh
@@ -1,2 +1,3 @@
 mkdir platformio-build
-platformio ci --verbose --board pro16MHzatmega328 --project-option="lib_extra_dirs=C:\Program Files (x86)\Arduino\libraries:upload_port=/dev/ttyAMA0" --build-dir=platformio-build --keep-build-dir led-ring
+chmod 666 /dev/ttyACM0
+platformio ci --verbose --board pro16MHzatmega328 --project-option="lib_extra_dirs=C:\Program Files (x86)\Arduino\libraries" --project-option="targets=upload" --project-option="upload_port=/dev/ttyUSB0" --build-dir=platformio-build --keep-build-dir led-ring
--- Build_win.bat
+++ Build_win.bat
@@ -1,4 +1,2 @@
 mkdir platformio-build
-platformio ci --verbose --board pro16MHzatmega328 --project-option="lib_extra_dirs=C:\Program Files (x86)\Arduino\libraries" --build-dir=platformio-build --keep-build-dir led-ring
-chmod 700 tools/mega_serial_start_win.py tools/avrdude_win.exe
-python tools/mega_serial_start_win.py
+platformio ci --verbose --board pro16MHzatmega328 --project-option="lib_extra_dirs=C:\Program Files (x86)\Arduino\libraries" --project-option="targets=upload" --project-option="upload_port=COM3" --build-dir=platformio-build --keep-build-dir led-ring
